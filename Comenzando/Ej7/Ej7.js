525d1e157b9bec63bb4f1fb9c2911c6da3fd4ffe
Carmen <carmenmartnromo@gmail.com>
2023-09-22 21:38:56 +0200

    Ej7 solo falta null

--- Comenzando/Ej7/Ej7.js
+++ Comenzando/Ej7/Ej7.js
@@ -4,27 +4,23 @@
 
 let st1;
 let num;
-let control;
+let control = true;
 
 
 do{
     st1 = prompt('Introduce un numero');
     num = parseInt(st1);
 
-    // console.log(num);
     if (isNaN(num)){
         console.log('No es un numero');
         continue;
     }
 
-    console.log('hola');
     if (num % 2 == 0){
         console.log('Es par');
-
     } else {
         console.log('Es impar');
     }
     control = false;
-
 } while (control);
 
